e7c33713bd875a35d8ca1663dc2ae96611dc90f9
angushushu <angushushu@outlook.com>
2025-11-18 00:02:37 -0500

    fix material issue

--- nms/versions/20_5/build.gradle.kts
+++ nms/versions/20_5/build.gradle.kts
@@ -8,17 +8,11 @@ java {
 
 repositories {
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
 }
 
 dependencies {
-    paperweight.paperDevBundle("1.20.5-R0.1-SNAPSHOT") {
-        exclude(group = "net.kyori")
-    }
+    paperweight.paperDevBundle("1.20.6-R0.1-SNAPSHOT")
     
-    // 手动添加稳定版本的 adventure 依赖
-    compileOnly("net.kyori:adventure-api:4.17.0")
-    compileOnly("net.kyori:adventure-text-minimessage:4.17.0")
-    compileOnly("net.kyori:adventure-text-serializer-gson:4.17.0")
-    compileOnly("net.kyori:adventure-text-serializer-legacy:4.17.0")
-    compileOnly("net.kyori:adventure-text-serializer-plain:4.17.0")
+    compileOnly(project(":nms:shared"))
 }
